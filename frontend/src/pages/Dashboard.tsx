c3ba3ef27f5afaa51791653e49ecae554b05e6ef
Arun Kumar <arun.kumar@stud-mail.uni-wuerzburg.de>
2025-08-22 13:28:39 +0200

    fix copilot

--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DashboardLayout from '../components/dashboard/DashboardLayout';
-
-const Dashboard: React.FC = () => {
-  return <DashboardLayout />;
-};
-
-export default Dashboard;
